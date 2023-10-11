16851432381f33b554c60a81d10b031a49641f63
XxSusuxX <gabriel.hneus@gmail.com>
2023-10-11 17:03:41 -0300

    Enviando a seção dispositivos disponiveis

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
   const buttons = document.querySelectorAll('[data-tab-button]');
 
   for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener('click', function(botao){
-      const abaAlvo = botao.target.dataset.tabButton;
-      const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-      escondeTodasAbas();
-      aba.classList.add('shows__list--is-active');
-      removeBotaoAtivo();
-      botao.target.classList.add('shows__tabs__button--is-active');
+    buttons[i].addEventListener('click', function(button){
+      const tabFocus = button.target.dataset.tabButton;
+      const tab = document.querySelector(`[data-tab-id=${tabFocus}]`);
+      hideAllTabs();
+      tab.classList.add('shows__list--is-active');
+      rmBtnActive();
+      button.target.classList.add('shows__tabs__button--is-active');
     });
   };
 
 });
 
-function removeBotaoAtivo(){
+function rmBtnActive(){
   const buttons = document.querySelectorAll('[data-tab-button]');
   for(let i = 0; i < buttons.length; i++){
     buttons[i].classList.remove('shows__tabs__button--is-active');
   };
 };
 
-function escondeTodasAbas(){
+function hideAllTabs(){
   const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
   for(let i = 0; i < tabsContainer.length; i++){
