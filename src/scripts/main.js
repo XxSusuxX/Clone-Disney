88732f6e4d92af6b8ea93d30f798cbe914e78ab3
XxSusuxX <gabriel.hneus@gmail.com>
2023-10-10 21:07:43 -0300

    Adicionando area de atracao

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,30 @@
-const axios = require('axios');
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('[data-tab-button]');
 
-const options = {
-  method: 'GET',
-  url: 'https://imdb8.p.rapidapi.com/auto-complete',
-  params: {q: 'game of thr'},
-  headers: {
-    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-  }
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(botao){
+      const abaAlvo = botao.target.dataset.tabButton;
+      const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+      escondeTodasAbas();
+      aba.classList.add('shows__list--is-active');
+      removeBotaoAtivo();
+      botao.target.classList.add('shows__tabs__button--is-active');
+    });
+  };
+
+});
+
+function removeBotaoAtivo(){
+  const buttons = document.querySelectorAll('[data-tab-button]');
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].classList.remove('shows__tabs__button--is-active');
+  };
 };
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+function escondeTodasAbas(){
+  const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+  for(let i = 0; i < tabsContainer.length; i++){
+    tabsContainer[i].classList.remove('shows__list--is-active');
+  };
+};
